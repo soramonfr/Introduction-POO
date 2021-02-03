ea18162053aa6f8079bfadfec37fc4bb4fd0f1b1
sorayacruchet <soraya.cruchet@gmail.com>
2021-02-03 18:59:30 +0100

    exo 1

--- models/Character.php
+++ models/Character.php
@@ -2,15 +2,6 @@
 class Character
 
 {
-    private $nickName;
     private $health;
     private $rage;
-   
-    public function __construct($nickName, $starterHealth = 100)
-    {
-        $this->nickName = $nickName;
-        $this->health = $starterHealth;
-        $this->rage = 0;
-    }
-
 }
