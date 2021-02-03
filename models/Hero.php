180540a5ca0181f5da482a4ff973e8ac5eb18400
sorayacruchet <soraya.cruchet@gmail.com>
2021-02-03 19:40:34 +0100

    exo 4&5

--- models/Hero.php
+++ models/Hero.php
@@ -1,2 +1,49 @@
 <?php
-class Hero
+class Hero extends Character
+
+{
+    private $weapon;
+    private $weaponDamage;
+    private $shield;
+    private $shieldValue;
+
+    public function getWeapon()
+    {
+        return $this->weapon;
+    }
+
+    public function setWeapon($weapon)
+    {
+        $this->weapon = $weapon;
+    }
+
+    public function getWeaponDamage()
+    {
+        return $this->weaponDamage;
+    }
+
+    public function setWeaponDamage($weaponDamage)
+    {
+        $this->weaponDamage = $weaponDamage;
+    }
+
+    public function getShield()
+    {
+        return $this->shield;
+    }
+
+    public function setShield($shield)
+    {
+        $this->shield = $shield;
+    }
+
+    public function getShieldValue()
+    {
+        return $this->shieldValue;
+    }
+
+    public function setShieldValue($shieldValue)
+    {
+        $this->shieldValue = $shieldValue;
+    }
+}
