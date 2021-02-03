7de5643a7a7d239c1d21822bd105502a4c22a29a
sorayacruchet <soraya.cruchet@gmail.com>
2021-02-03 19:29:11 +0100

    exo 2&3

--- models/Character.php
+++ models/Character.php
@@ -4,4 +4,26 @@ class Character
 {
     private $health;
     private $rage;
+    
+    public function __construct($health = 100)
+    {
+        $this->health = $health;
+        $this->rage = 0;
+    }
+
+    public function getHealth() {
+        return $this->health;
+    }
+
+    public function setHealth($health) {
+        $this->health = $health;
+    }
+
+    public function getRage() {
+        return $this->rage;
+    }
+
+    public function setRage($rage) {
+        $this->rage = $rage;
+    }
 }
